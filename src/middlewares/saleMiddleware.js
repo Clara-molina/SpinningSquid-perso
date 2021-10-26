b18cee22eeeb697169cf0fee7e683cc20c2cda26
RomainTalbot <romain.talbot.33@gmail.com>
2021-10-26 11:39:06 +0200

    add a loading sale post's method

--- src/middlewares/saleMiddleware.js
+++ src/middlewares/saleMiddleware.js
@@ -4,4 +4,13 @@ const saleMiddleware = {
 
   //! A changer
   baseURI: 'http://localhost/SpinningSquad_Apotheose/projet-skatepark/public/wp-json/wp/v2/sale',
-}
+
+  endPointSale: 'sale?_embed=true',
+
+  // Méthode permettant de récupérer tous les post sale
+  async loadSalePost() {
+    const response = await axios.get(saleMiddleware.baseURI + saleMiddleware.endPointSale);
+
+    return response.data;
+  },
+};
