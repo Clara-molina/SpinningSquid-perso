b68632d091628cc2d0a8568ff80e806b613a8a6d
ThomasBOQUET <thomasboquet@hotmail.fr>
2021-10-27 14:32:43 +0200

    placing a basic html&css star rating passive function

--- src/components/SearchSkateResults/index.js
+++ src/components/SearchSkateResults/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import GoogleMapReact from 'google-map-react';
-
-
+import Img from '../../assets/img/Skateparktest.jpg';
+import './searchSkateResults.scss';
 
 const SearchSkateResults = () => (
-  <h3>blabla</h3>
+  <div className="searchresult">
+    <h3 className="searchresulttitle">Résultat</h3>
+    <h4 className="searchresultname">Skatepark Achères</h4>
+    <img className="searchresultimage" src={Img} alt="skatepark Achères" />
+    <span className="searchresultstars">Etoiles</span>
+
+    <span className="star__container">
+      <input type="radio" name="rating" value="1" id="star-1" className="star__radio visuhide" />
+      <input type="radio" name="rating" value="2" id="star-2" className="star__radio visuhide" />
+      <input type="radio" name="rating" value="3" id="star-3" className="star__radio visuhide" />
+      <input type="radio" name="rating" value="4" id="star-4" className="star__radio visuhide" />
+      <input type="radio" name="rating" value="5" id="star-5" className="star__radio visuhide" />
+
+      <span className="star__item" htmlFor="star-1"><span className="visuhide">1 stars</span></span>
+      <span className="star__item" htmlFor="star-2"><span className="visuhide">2 stars</span></span>
+      <span className="star__item" htmlFor="star-3"><span className="visuhide">3 stars</span></span>
+      <span className="star__item" htmlFor="star-4"><span className="visuhide">4 stars</span></span>
+      <span className="star__item" htmlFor="star-5"><span className="visuhide">5 stars</span></span>
+    </span>
+
+    <span className="searchresultadress">Adresse : 227 Route Départementale 30, 78260 Achères</span>
+    <span className="detailssearchresult">Parking</span>
+    <span className="detailssearchresult">Poubelles</span>
+
+    <div className="searchresultmap">Map de l'adresse</div>
+  </div>
+
 );
 
 export default SearchSkateResults;
-
