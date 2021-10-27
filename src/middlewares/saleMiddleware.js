d74c0fb568d1890674c28be80c52cc09f106d969
RomainTalbot <romain.talbot.33@gmail.com>
2021-10-27 14:47:08 +0200

    add methods for display all skatepark and articles

--- src/middlewares/saleMiddleware.js
+++ src/middlewares/saleMiddleware.js
@@ -6,10 +6,26 @@ const saleMiddleware = {
   baseURI: 'http://romain-talbot.vpnuser.lan/SpinningSquad_Apotheose/projet-skatepark/public/wp-json/wp/v2/',
 
   endPointSale: 'sale?_embed=true',
+  endPointSkatepark: 'skatepark?_embed=true',
+  endPointArticle: 'article?_embed=true',
 
   // Méthode permettant de récupérer tous les post sale
   async loadSalePost() {
     const response = await axios.get(saleMiddleware.baseURI + saleMiddleware.endPointSale);
+
+    return response.data;
+  },
+
+  // Méthode permettant de récupérer tous les post skatepark
+  async loadSkateparkPost() {
+    const response = await axios.get(saleMiddleware.baseURI + saleMiddleware.endPointSkatepark);
+
+    return response.data;
+  },
+
+  // Méthode permettant de récupérer tous les post article
+  async loadArticlePost() {
+    const response = await axios.get(saleMiddleware.baseURI + saleMiddleware.endPointArticle);
 
     return response.data;
   },
