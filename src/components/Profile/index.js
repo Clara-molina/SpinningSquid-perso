eb567ce58479cd42695604b08fd0ce773b372220
RomainTalbot <romain.talbot.33@gmail.com>
2021-11-18 11:21:48 +0100

    responsive user page

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -17,138 +17,140 @@ const Profile = (props) => {
       <div className="profile-container">
         <img className="profile-avatar" src={logo2} alt="avatar" />
         <p className="profile-name">{props.usernameConnection}</p>
-        <Popup
-          trigger={
-            <button className="button profile-button-edit" type="button">
-              Editer mon profil
-            </button>
-          }
-          modal
-          nested
-        >
-          {(close) => (
-            <div className="modal">
-              <div className="header"> Modifie ton profil </div>
-              <form className="form" onSubmit={props.handleSubmitUpdateProfile}>
-                <div className="content">
-                  <label className="form-edit-label" htmlFor="usernameEdit">
-                    Nom d'utilisateur
-                    <input
-                      id="usernameEdit"
-                      type="text"
-                      className="form-edit-input"
-                      value={props.usernameEdit}
-                      // required
-                      onChange={props.handleChange}
-                    />
-                  </label>
-                  <label className="form-edit-label" htmlFor="lastnameEdit">
-                    Nom
-                    <input
-                      id="lastnameEdit"
-                      type="text"
-                      className="form-edit-input"
-                      value={props.lastnameEdit}
-                      // required
-                      onChange={props.handleChange}
-                    />
-                  </label>
-                  <label className="form-edit-label" htmlFor="firstnameEdit">
-                    Prénom
-                    <input
-                      id="firstnameEdit"
-                      type="text"
-                      className="form-edit-input"
-                      value={props.firstnameEdit}
-                      // required
-                      onChange={props.handleChange}
-                    />
-                  </label>
-                  <label className="form-edit-label" htmlFor="streetEdit">
-                    Rue
-                    <input
-                      id="streetEdit"
-                      type="text"
-                      className="form-edit-input"
-                      value={props.streetEdit}
-                      // required
-                      onChange={props.handleChange}
-                    />
-                  </label>
-                  <label className="form-edit-label" htmlFor="postalEdit">
-                    Code postal
-                    <input
-                      id="postalEdit"
-                      type="text"
-                      className="form-edit-input"
-                      value={props.postalEdit}
-                      // required
-                      onChange={props.handleChange}
-                    />
-                  </label>
-                  <label className="form-edit-label" htmlFor="cityEdit">
-                    Ville
-                    <input
-                      id="cityEdit"
-                      type="text"
-                      className="form-edit-input"
-                      value={props.cityEdit}
-                      // required
-                      onChange={props.handleChange}
-                    />
-                  </label>
-                  <label className="form-edit-label" htmlFor="emailEdit">
-                    Email
-                    <input
-                      id="emailEdit"
-                      type="email"
-                      className="form-edit-input"
-                      value={props.emailEdit}
-                      // required
-                      onChange={props.handleChange}
-                    />
-                  </label>
-                  <label className="form-edit-label" htmlFor="passwordEdit">
-                    Password
-                    <input
-                      id="passwordEdit"
-                      type="password"
-                      className="form-edit-input"
-                      value={props.passwordEdit}
-                      // required
-                      onChange={props.handleChange}
-                    />
-                  </label>
-                  <label className="form-edit-label" htmlFor="passwordEdit">
-                    Avatar
-                    <input
-                      id="avatarImage"
-                      type="file"
-                      className="form-edit-input"
-                      // required
-                      onChange={props.handleUploadLocalImage}
-                    />
-                  </label>
-                </div>
-                <div className="actions">
-                  <button type="submit" className="button">
-                    Valider
-                  </button>
-                  <button
-                    type="button"
-                    className="button"
-                    onClick={() => {
-                      close();
-                    }}
-                  >
-                    Annuler
-                  </button>
-                </div>
-              </form>
-            </div>
-          )}
-        </Popup>
-        <button className="button profile-button-edit" onClick={props.handleDelete}>Supprimer compte</button>
+        <div className="profile-button-dock">
+          <Popup
+            trigger={
+              <button className="button" type="button">
+                Editer mon profil
+              </button>
+            }
+            modal
+            nested
+          >
+            {(close) => (
+              <div className="modal">
+                <div className="header"> Modifie ton profil </div>
+                <form className="form" onSubmit={props.handleSubmitUpdateProfile}>
+                  <div className="content">
+                    <label className="form-edit-label" htmlFor="usernameEdit">
+                      Nom d'utilisateur
+                      <input
+                        id="usernameEdit"
+                        type="text"
+                        className="form-edit-input"
+                        value={props.usernameEdit}
+                        // required
+                        onChange={props.handleChange}
+                      />
+                    </label>
+                    <label className="form-edit-label" htmlFor="lastnameEdit">
+                      Nom
+                      <input
+                        id="lastnameEdit"
+                        type="text"
+                        className="form-edit-input"
+                        value={props.lastnameEdit}
+                        // required
+                        onChange={props.handleChange}
+                      />
+                    </label>
+                    <label className="form-edit-label" htmlFor="firstnameEdit">
+                      Prénom
+                      <input
+                        id="firstnameEdit"
+                        type="text"
+                        className="form-edit-input"
+                        value={props.firstnameEdit}
+                        // required
+                        onChange={props.handleChange}
+                      />
+                    </label>
+                    <label className="form-edit-label" htmlFor="streetEdit">
+                      Rue
+                      <input
+                        id="streetEdit"
+                        type="text"
+                        className="form-edit-input"
+                        value={props.streetEdit}
+                        // required
+                        onChange={props.handleChange}
+                      />
+                    </label>
+                    <label className="form-edit-label" htmlFor="postalEdit">
+                      Code postal
+                      <input
+                        id="postalEdit"
+                        type="text"
+                        className="form-edit-input"
+                        value={props.postalEdit}
+                        // required
+                        onChange={props.handleChange}
+                      />
+                    </label>
+                    <label className="form-edit-label" htmlFor="cityEdit">
+                      Ville
+                      <input
+                        id="cityEdit"
+                        type="text"
+                        className="form-edit-input"
+                        value={props.cityEdit}
+                        // required
+                        onChange={props.handleChange}
+                      />
+                    </label>
+                    <label className="form-edit-label" htmlFor="emailEdit">
+                      Email
+                      <input
+                        id="emailEdit"
+                        type="email"
+                        className="form-edit-input"
+                        value={props.emailEdit}
+                        // required
+                        onChange={props.handleChange}
+                      />
+                    </label>
+                    <label className="form-edit-label" htmlFor="passwordEdit">
+                      Password
+                      <input
+                        id="passwordEdit"
+                        type="password"
+                        className="form-edit-input"
+                        value={props.passwordEdit}
+                        // required
+                        onChange={props.handleChange}
+                      />
+                    </label>
+                    <label className="form-edit-label" htmlFor="passwordEdit">
+                      Avatar
+                      <input
+                        id="avatarImage"
+                        type="file"
+                        className="form-edit-input"
+                        // required
+                        onChange={props.handleUploadLocalImage}
+                      />
+                    </label>
+                  </div>
+                  <div className="actions">
+                    <button type="submit" className="button">
+                      Valider
+                    </button>
+                    <button
+                      type="button"
+                      className="button"
+                      onClick={() => {
+                        close();
+                      }}
+                    >
+                      Annuler
+                    </button>
+                  </div>
+                </form>
+              </div>
+            )}
+          </Popup>
+          <button className="button" onClick={props.handleDelete}>Supprimer compte</button>
+        </div>
       </div>
 
       <div className="profile-contribution">
@@ -161,22 +163,24 @@ const Profile = (props) => {
             return (
               <>
                 <h3 className="profile-articles-title">{item.title.rendered}</h3>
-                <Link to={urlSkateId}>
-                  <button
-                    className="button button-articles"
-                    type="button"
-                  >
-                    Consulter
-                  </button>
-                </Link>
-                <Link to={urlSkateEditId}>
-                  <button
-                    className="button button-articles"
-                    type="button"
-                  >
-                    Modifier
-                  </button>
-                </Link>
+                <div className="profile-articles-button-dock">
+                  <Link to={urlSkateId}>
+                    <button
+                      className="button"
+                      type="button"
+                    >
+                      Consulter
+                    </button>
+                  </Link>
+                  <Link to={urlSkateEditId}>
+                    <button
+                      className="button"
+                      type="button"
+                    >
+                      Modifier
+                    </button>
+                  </Link>
+                </div>
               </>
             )
           })}
@@ -188,21 +192,23 @@ const Profile = (props) => {
             return (
               <>
                 <h3 className="profile-articles-title">{item.title.rendered}</h3>
-                <Link to={urlSaleId}>
+                <div className="profile-articles-button-dock">
+                  <Link to={urlSaleId}>
+                    <button
+                      className="button"
+                      type="button"
+                    >
+                      Consulter
+                    </button>
+                  </Link>
                   <button
-                    className="button button-articles"
+                    className="button"
                     type="button"
+                    to=""
                   >
-                    Consulter
+                    Modifier
                   </button>
-                </Link>
-                <button
-                  className="button button-articles"
-                  type="button"
-                  to=""
-                >
-                  Modifier
-                  </button>
+                </div>
               </>
             )
           })}
@@ -214,22 +220,24 @@ const Profile = (props) => {
             return (
               <>
                 <h3 className="profile-articles-title">{item.title.rendered}</h3>
-                <Link to={urlArticleId}>
+                <div className="profile-articles-button-dock">
+                  <Link to={urlArticleId}>
+                    <button
+                      className="button button-articles"
+                      type="button"
+                      to="/communaute/"
+                    >
+                      Consulter
+                    </button>
+                  </Link>
                   <button
                     className="button button-articles"
                     type="button"
-                    to="/communaute/"
+                    to=""
                   >
-                    Consulter
-                  </button>
-                </Link>
-                <button
-                  className="button button-articles"
-                  type="button"
-                  to=""
-                >
-                  Modifier
-                  </button>
+                    Modifier
+                    </button>
+                </div>
               </>
             )
           })}
